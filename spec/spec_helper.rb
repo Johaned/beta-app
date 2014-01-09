1f5eda2ea76d80b6d7d559e89a34b9a7ff3def8d
Johan Tique <johan.tique@codescrum.com>
2014-01-09 18:06:10 -0500

    add customize rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,38 +1,89 @@
+require 'rubygems'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+# Loading more in this block will cause your tests to run faster. However,
+# if you change any configuration or code from libraries loaded here, you'll
+# need to restart spork for it take effect.
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'simplecov'
+require 'simplecov-rcov'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+  SimpleCov.start 'rails'
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # From https://github.com/plataformatec/devise/wiki/How-To:-Controllers-tests-with-Rails-3-(and-rspec)
+  config.include Devise::TestHelpers, :type => :controller
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  # See https://github.com/plataformatec/devise/wiki/How-To:-Test-with-Capybara
+  config.include Warden::Test::Helpers, :type => :request
+  Warden.test_mode!
 
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
   config.order = "random"
+
+  config.before(:suite) do
+    # p "BEFORE SUITE"
+    # p "Setting the databse cleaner to mongoid with truncation"
+    DatabaseCleaner[:mongoid].strategy = :truncation
+  end
+
+  config.after(:suite) do
+    # p "AFTER SUITE"
+    # p "DatabaseCleaner.clean"
+    DatabaseCleaner.clean
+  end
+
+  config.include(MailerMacros)
+
+  config.before(:each) do |x|
+    if x.example.metadata[:demo_data].blank? # Only do this if not using demo data
+      # p "BEFORE EACH: DatabaseCleaner.clean and make a Tenant and Medic Role"
+      DatabaseCleaner.start # does nothing with Mongoid but is included in DatabaseCleaner for consistency
+      DatabaseCleaner.clean # clean before rather than after in case run after a demo_data test
+      #reset_email
+      #
+      ## basic seed data for integration tests
+      #Tenant.make!(:name => 'superadmin')
+      #Tenant.current = Tenant.make!(:hosts => ["127.0.0.1"])
+      #Role.make!(:medic)
+      #Role.make!(:main_contact)
+      #Role.make!(:admin)
+      #Role.make!(:cd)
+      #Role.make!(:lead)
+      #Role.make!(:ro)
+      #Role.make!(:medical_director)
+      #Role.make!(:appraiser)
+      #Role.make!(:msf_admin)
+      #Role.make!(:jp_admin)
+      #Role.make!(:speciality_lead)
+    end
+  end
+
+  config.before(:all, :demo_data => true) do
+    # p "BEFORE ALL DEMO DATA: Drop test and then populate Demo Data"
+    if Mongoid.master.name == "sard_test_full"
+      p "Dropping all collection in #{Mongoid.master.name}"
+      Mongoid.master.collections.reject {|c| c.name =~ /^system/}.each(&:drop)
+      sh "bundle exec mongorestore -h localhost #{Rails.root}/spec/assets/test_database"
+    else
+      p "Refusing to drop the database unless it's sard_test_full"
+    end
+  end
+
+  Capybara.default_host = "http://127.0.0.1"
+  Capybara.javascript_driver = :webkit
+  #Capybara.server_port = 3000
+  # Capybara.app_host = "http://127.0.0.1"
+  # Capybara.server_host = "http://127.0.0.1"
 end
