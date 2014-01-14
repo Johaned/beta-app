f054c3bbc88e0caf861b87aad6a6337e8c05dc20
Johaned <johan.tique@codescrum.com>
2014-01-13 22:12:11 -0500

    add another embedded_in relation in RelationData model

--- app/models/edoc.rb
+++ app/models/edoc.rb
@@ -3,18 +3,21 @@ class Edoc
   field :name, :type => String
   field :description, :type => String
 
-  has_many :tags, as: :taggable
+  embeds_many :dogs, class_name: 'RelationData', cascade_callbacks: true
+  accepts_nested_attributes_for :dogs
 
-  def tags_from_form=(raw_tags)
-    #tags = []
-    raw_tags.gsub(',').each do |label|
-      self.tags << Tag.new(label: label)
-    end
-    binding.pry
-  end
-
-  def tags_from_form
-    tags.to_a.map {|tag| tag.label}.join ','
-  end
+  #has_many :tags, as: :taggable
+  #
+  #def tags_from_form=(raw_tags)
+  #  #tags = []
+  #  raw_tags.gsub(',').each do |label|
+  #    self.tags << Tag.new(label: label)
+  #  end
+  #  binding.pry
+  #end
+  #
+  #def tags_from_form
+  #  tags.to_a.map {|tag| tag.label}.join ','
+  #end
 
 end
--- app/models/relation_data.rb
+++ app/models/relation_data.rb
@@ -3,4 +3,5 @@ class RelationData
   field :association_id, type: String
 
   embedded_in :tagging, class_name: 'Tagging', inverse_of: :cows
+  embedded_in :edoc, class_name: 'Edog', inverse_of: :dogs
 end
